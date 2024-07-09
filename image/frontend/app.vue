c225f5b3e2093088ce9435b59f0653a30b0ef6c5
ymjoz <rossiwhite@duck.com>
2024-07-09 11:25:27 +0800

    前端先新增一個nuxt3初稿

---
+++ image/frontend/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import AppAlert from './components/AppAlert.vue';
+</script>
+
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <h1>
+      Hello, Shikoku!
+    </h1>
+    <AppAlert>
+      <strong>Alert:</strong>
+      This is an alert!
+    </AppAlert>
+  </div>
+</template>
