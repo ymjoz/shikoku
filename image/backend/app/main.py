847ae69a5d1d3c3dfcf5700bb2203cdfe6f7cb5b
ymjoz <rossiwhite@duck.com>
2024-07-09 09:47:41 +0800

    先完成一個ARM64架構的backend Docker image

--- image/backend/app/main.py
+++ image/backend/app/main.py
@@ -1,21 +1,21 @@
-# from .version import VERSION
+from .version import VERSION
 from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get("/", status_code=200, summary="回傳程式版本資訊，可用於確認伺服器活著.")
-# def root():
-#     """
-#     回傳程式版本資訊，可用於確認伺服器活著.
-#     """
-#     return {"msg": "server is ready.", "version": VERSION}
+@app.get("/", status_code=200, summary="回傳程式版本資訊，可用於確認伺服器活著.")
+def root():
+    """
+    回傳程式版本資訊，可用於確認伺服器活著.
+    """
+    return {"msg": "server is ready.", "version": VERSION}
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# async def read_root():
+#     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
