2638c1381118653c2c47e987b7854922d2b41564
ymjoz <rossiblack@duck.com>
2024-08-01 20:54:30 +0800

    chore:some stuff

--- image/frontend4/index.js
+++ image/frontend4/index.js
@@ -2,7 +2,8 @@ const playList = {
   data() {
     return {
       rossiValue: 100,
-      playlist: ['命運交響曲', '小夜曲', 'Blinding Lights', 'Shape of You', 'Cello Suite No. 1 in G Major, BWV 1007', 'Clair de Lune', ],
+      // playlist: ['命運交響曲', '小夜曲', 'Blinding Lights', 'Shape of You', 'Cello Suite No. 1 in G Major, BWV 1007', 'Clair de Lune', ],
+      playlist: ['命運交響曲', '小夜曲', 'Blinding Lights', 'Shape of You', '千里共蟬娟', 'Clair de Lune', ],
       searchTerm: '',
     }
   },
