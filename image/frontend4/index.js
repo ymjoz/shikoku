433c9926ea9a04d4435c0b687af7232998c11c28
ymjoz <rossiblack@duck.com>
2024-08-03 10:35:37 +0800

    chore:frontend4 small edits

--- image/frontend4/index.js
+++ image/frontend4/index.js
@@ -3,9 +3,18 @@ const playList = {
     return {
       rossiValue: 100,
       // playlist: ['命運交響曲', '小夜曲', 'Blinding Lights', 'Shape of You', 'Cello Suite No. 1 in G Major, BWV 1007', 'Clair de Lune', ],
-      playlist: ['命運交響曲', '小夜曲', 'Blinding Lights', 'Shape of You', '千里共蟬娟', 'Clair de Lune', ],
+      playlist: [
+        '命運交響曲',
+        '小夜曲',
+        'Blinding Lights',
+        'Shape of You',
+        '千里共蟬娟',
+        'Clair de Lune',
+        '月光',
+        'Cello Suite No. 1 in G Major, BWV 1007',
+      ],
       searchTerm: '',
-    }
+    };
   },
 
   computed: {
@@ -15,15 +24,14 @@ const playList = {
           return song.toLowerCase().includes(this.searchTerm.toLowerCase());
         });
       } else {
-        return this.playlist;
+        // return this.playlist;
+        return [];
       }
     },
     testFunc() {
       return this.rossiValue;
-    }
+    },
   },
-
-}
+};
 
 Vue.createApp(playList).mount('#app');
-
