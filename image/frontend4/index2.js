47fed53c0ca180778511509c4246d31f2342fdec
ymjoz <rossiblack@duck.com>
2024-08-07 20:56:13 +0800

    feat:自訂WordCountApp組件

--- image/frontend4/index2.js
+++ image/frontend4/index2.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({});
 
 app.component('WordCount', {
   template: `<div>
-    <h3>Word Count</h3>
-    <textarea v-model="content"></textarea>
+    <h3>字數計算</h3>
+    <textarea v-model="content" placeholder="請輸入任意字"></textarea>
     <p>你輸入了 {{ count }} 個字</p>
   </div>`,
   data() {
@@ -16,6 +16,13 @@ app.component('WordCount', {
       return this.content.trim().length;
     },
   },
+});
+
+app.component('WordCountApp', {
+  template: `<div>
+    <h1>Word Count App</h1>
+    <WordCount />
+  </div>`,
 });
 
 const vm = app.mount('#app');
