3fb7f3f9a3993636069137ea48f9152b659c0bdc
ymjoz <rossiblack@duck.com>
2024-08-07 19:42:13 +0800

    feat:自訂component word-count

--- image/frontend4/index2.js
+++ image/frontend4/index2.js
@@ -1,7 +1,21 @@
-const app = Vue.createApp({
+const app = Vue.createApp({});
+
+app.component('WordCount', {
+  template: `<div>
+    <h3>Word Count</h3>
+    <textarea v-model="content"></textarea>
+    <p>你輸入了 {{ count }} 個字</p>
+  </div>`,
   data() {
     return {
-      vueName: 'CSW',
+      content: '',
     };
   },
+  computed: {
+    count() {
+      return this.content.trim().length;
+    },
+  },
 });
+
+const vm = app.mount('#app');
